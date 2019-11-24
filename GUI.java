ce79395dce25f8d596e58fbecf90702d0eb33199
randybaldeo1024 <58125413+randybaldeo1024@users.noreply.github.com>
2019-11-23 21:00:06 -0500

    Update GUI.java

--- GUI.java
+++ GUI.java
@@ -4,10 +4,10 @@ public class GUI extends JFrame {
 	public GUI() {
 		setTitle ("Airline GUI 1");
         setSize (500, 500);
-        setLocation (400, 100);
+        setLocation (400, 100); 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);	
-        
+        //adding this comment after file was uploaded to github - Randy B.
 		
 	}
 
