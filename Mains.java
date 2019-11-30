5636ecf592ea5f2143e9f0177b72b6f5c69adcfa
randybaldeo1024 <58125413+randybaldeo1024@users.noreply.github.com>
2019-11-29 19:22:51 -0500

    Update Mains.java

--- Mains.java
+++ Mains.java
@@ -1,5 +1,6 @@
 
 public class Mains {
+	public static String date = "11/29/2019";
 	public static void main(String args[]) {
 		SearchGUI se = new SearchGUI();
 		SearchGUI.btn.addActionListener(se);
@@ -7,6 +8,9 @@ public class Mains {
 		CustSigIn cl = new CustSigIn();
 		CustSigIn.Login.addActionListener(cl);
 		CustSigIn.NewUser.addActionListener(cl);
+		
+		CustNewGUI cn = new CustNewGUI();
+		CustNewGUI.Create.addActionListener(cn);
 		
 	}
 }
