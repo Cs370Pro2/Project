d283166868646382e5833226f6b96fe71efc40c8
randybaldeo1024 <58125413+randybaldeo1024@users.noreply.github.com>
2019-12-01 18:55:41 -0500

    Update Mains.java

--- Mains.java
+++ Mains.java
@@ -2,15 +2,22 @@
 public class Mains {
 	public static String date = "11/29/2019";
 	public static void main(String args[]) {
-		SearchGUI se = new SearchGUI();
-		SearchGUI.btn.addActionListener(se);
-		
 		CustSigIn cl = new CustSigIn();
 		CustSigIn.Login.addActionListener(cl);
 		CustSigIn.NewUser.addActionListener(cl);
 		
-		CustNewGUI cn = new CustNewGUI();
-		CustNewGUI.Create.addActionListener(cn);
+		SearchGUI se = new SearchGUI();
+		SearchGUI.btn.addActionListener(se);
 		
+//		CustSigIn cl = new CustSigIn();
+//		CustSigIn.Login.addActionListener(cl);
+//		CustSigIn.NewUser.addActionListener(cl);
+		
+		AirlineGUI ag = new AirlineGUI();
+		
+		//moved to Customer Sign GUI
+//		CustNewGUI cn = new CustNewGUI();
+//		CustNewGUI.Create.addActionListener(cn);
+				
 	}
 }
