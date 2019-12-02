8845ac923b0247ce174e6440b5d3067712d64a9a
randybaldeo1024 <58125413+randybaldeo1024@users.noreply.github.com>
2019-12-02 11:24:13 -0500

    Update Mains.java

--- Mains.java
+++ Mains.java
@@ -1,23 +1,51 @@
+import javax.swing.JOptionPane;
 
 public class Mains {
-	public static String date = "11/29/2019";
+	public static String month,day,year,date;
+	public static int intmonth,intday,intyear;
+	public static boolean isadmin = false;
+//	public static String date = "11/29/2019";
 	public static void main(String args[]) {
+		month = JOptionPane.showInputDialog("Input month:");
+		intmonth = Integer.parseInt(month);
+		day = JOptionPane.showInputDialog("Input day:");
+		intday = Integer.parseInt(day);
+		year = JOptionPane.showInputDialog("Input year:");
+		intyear = Integer.parseInt(year);
+		date = ("Current date: " + month + "/" + day + "/" + year);
+
+		//Opens Sign-In Page
 		CustSigIn cl = new CustSigIn();
 		CustSigIn.Login.addActionListener(cl);
 		CustSigIn.NewUser.addActionListener(cl);
 		
+		//Opens Search Engine
 		SearchGUI se = new SearchGUI();
 		SearchGUI.btn.addActionListener(se);
 		
-//		CustSigIn cl = new CustSigIn();
-//		CustSigIn.Login.addActionListener(cl);
-//		CustSigIn.NewUser.addActionListener(cl);
+		//Opens FlightRaid GUI
+		FlightRaidGUI fr = new FlightRaidGUI();
+		FlightRaidGUI.cancelflight1.addActionListener(fr);
+		FlightRaidGUI.cancelflight2.addActionListener(fr);
+		FlightRaidGUI.cancelflight3.addActionListener(fr);
+		FlightRaidGUI.insert.addActionListener(fr);
+		FlightRaidGUI.insert2.addActionListener(fr);
 		
-		AirlineGUI ag = new AirlineGUI();
+		//Opens Red Bird GUI
+		RedBirdGUI rb = new RedBirdGUI();
+		RedBirdGUI.cancelflight1.addActionListener(rb);
+		RedBirdGUI.cancelflight2.addActionListener(rb);
+		RedBirdGUI.cancelflight3.addActionListener(rb);
+		RedBirdGUI.insert.addActionListener(rb);
+		RedBirdGUI.insert2.addActionListener(rb);
 		
-		//moved to Customer Sign GUI
-//		CustNewGUI cn = new CustNewGUI();
-//		CustNewGUI.Create.addActionListener(cn);
-				
+		//Opens Humming Dart GUI
+		HummingDartGUI hd = new HummingDartGUI();
+		HummingDartGUI.cancelflight1.addActionListener(hd);
+		HummingDartGUI.cancelflight2.addActionListener(hd);
+		HummingDartGUI.cancelflight3.addActionListener(hd);
+		HummingDartGUI.insert.addActionListener(hd);
+		HummingDartGUI.insert2.addActionListener(hd);
+
 	}
 }
